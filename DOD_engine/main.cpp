9fb14042f1eb9bb15cff4bdf3b6d1418e40e9daf
Rafo Bitro <67033048+rafobitro@users.noreply.github.com>
2026-03-26 20:30:26 -0700

    Update main.cpp
    1.precomputed sin and cos incresed a performance 2 times
    2. add a freeing a alocation and end state
    3. add a window and render resising

--- DOD_engine/main.cpp
+++ DOD_engine/main.cpp
@@ -1,6 +1,6 @@
 #define PI 3.1415926535f
 #define GRID_SIZE 64
-#define TEXTURE_SIZE 128
+#define TEXTURE_SIZE 64
 //16 by 9 ratio
 #define RENDER_W 800
 #define RENDER_H 450
@@ -14,6 +14,8 @@
 #include <cstdint>
 #include <cmath>
 #include <chrono>
+
+bool running = true;
 
 
 
@@ -63,6 +65,8 @@ void init_Player(Player& player) {
 }
 
 struct Game_state {
+    int render_w= RENDER_W;
+    int render_h= RENDER_H;
     Player player;
     Textures textures;
     uint32_t* framebuffer;
@@ -95,23 +99,26 @@ struct Game_state {
 
 void render(Game_state& state) {
     float ray_angle  = (state.player.angle - HORIZONTAL_FOV / 2.0f);
-    float angle_step = (1.0f / RENDER_W) * HORIZONTAL_FOV;
+    float angle_step = (1.0f / state.render_w) * HORIZONTAL_FOV;
     float ray_x, ray_y;
     int distance_to_wall;
     bool hit_vertical_wall;
     //raycaster
 
-    for (int i = 0;i < RENDER_W;i++) {
+    for (int i = 0;i < state.render_w;i++) {
         ray_angle += angle_step;
         ray_x = state.player.x;
         ray_y = state.player.y;
         distance_to_wall = 0;
         hit_vertical_wall = false;
+        float ray_rad = ray_angle * PI / 180.0f;
+        float ray_cos = cos(ray_rad);
+        float ray_sin = sin(ray_rad);
 
         while (state.map[(int)ray_y / GRID_SIZE][(int)ray_x / GRID_SIZE] == 0) {
             distance_to_wall++;
-            float next_x = ray_x + std::cos(ray_angle * PI / 180.0f);
-            float next_y = ray_y + std::sin(ray_angle * PI / 180.0f);
+            float next_x = ray_x + ray_cos;
+            float next_y = ray_y + ray_sin;
 
             if ((int)next_x / GRID_SIZE != (int)ray_x / GRID_SIZE)
                 hit_vertical_wall = true;
@@ -123,8 +130,8 @@ void render(Game_state& state) {
         }
 		
         int total_visable_hight = distance_to_wall * std::tan((VERTICAL_FOV/2)*PI/180.f);
-		int Wall_pixels = RENDER_H * GRID_SIZE / total_visable_hight;
-        int ceilling_pixels = (RENDER_H / 2) - (Wall_pixels / 2 );
+		int Wall_pixels = state.render_h * GRID_SIZE / total_visable_hight;
+        int ceilling_pixels = (state.render_h / 2) - (Wall_pixels / 2 );
         
 
 
@@ -136,7 +143,12 @@ void render(Game_state& state) {
             tex_x = (int)TEXTURE_SIZE * ((ray_y/GRID_SIZE) - floor((ray_y / GRID_SIZE)));
 
 
-        for (int j = 0;j < RENDER_H; j++) {
+
+         ray_rad = ray_angle * PI / 180.0f;
+        ray_cos = cos(ray_angle * PI / 180.0f);
+         ray_sin = sin(ray_angle * PI / 180.0f);
+
+        for (int j = 0;j < state.render_h; j++) {
             float fade = 1.0f-(distance_to_wall/500.0f);
             if (fade < 0) fade = 0;
 
@@ -145,7 +157,7 @@ void render(Game_state& state) {
 
             if (j < ceilling_pixels) {
 
-                distance = (0.5f*distance_to_wall)/((RENDER_H / 2.0f -j)/Wall_pixels);
+                distance = (0.5f*distance_to_wall)/((state.render_h / 2.0f -j)/Wall_pixels);
 
                 fade = 1.0f - (distance / 500.0f);
                 if (fade < 0) fade = 0;
@@ -155,8 +167,8 @@ void render(Game_state& state) {
                 float angle = state.player.angle - ray_angle;
                 int ciling_tex_x;
                 int ciling_tex_y;
-                x_cord = state.player.x + (int)(std::cos(ray_angle * PI / 180.0f) * distance);
-                y_cord = state.player.y + (int)(std::sin(ray_angle * PI / 180.0f) * distance);
+                x_cord = state.player.x + (int)(ray_cos * distance);
+                y_cord = state.player.y + (int)(ray_sin * distance);
 
                 ciling_tex_x = (int)TEXTURE_SIZE * ((x_cord / GRID_SIZE) - floor((x_cord / GRID_SIZE)));
 
@@ -176,7 +188,7 @@ void render(Game_state& state) {
                 b = (uint8_t)b * fade;
 
 
-                state.framebuffer[i + RENDER_W * j] = r << 16 | g << 8 | b;
+                state.framebuffer[i + state.render_w * j] = r << 16 | g << 8 | b;
 
       
             }
@@ -197,12 +209,12 @@ void render(Game_state& state) {
                 b = (uint8_t)b* fade;
 
 
-                state.framebuffer[i + RENDER_W * j] = r << 16|g<<8 |b;
+                state.framebuffer[i + state.render_w * j] = r << 16|g<<8 |b;
 
             }
             else {
               
-                distance = (0.5f * distance_to_wall) / ((j-RENDER_H / 2.0f) / Wall_pixels);
+                distance = (0.5f * distance_to_wall) / ((j-state.render_h / 2.0f) / Wall_pixels);
 
                 fade = 1.0f - (distance / 500.0f);
                 if (fade < 0) fade = 0;
@@ -233,7 +245,7 @@ void render(Game_state& state) {
                 b = (uint8_t)b *= fade ;
 
 
-                state.framebuffer[i + RENDER_W * j] = r << 16 | g << 8 | b;
+                state.framebuffer[i + state.render_w * j] = r << 16 | g << 8 | b;
 
                     
                     
@@ -322,6 +334,7 @@ void generate_gradient_texture (uint32_t* bitmap) {
             uint8_t blue = (uint8_t)(255.0f * (1.0f - std::sqrt(fx * fx + (1 - fy) * (1 - fy)) / std::sqrt(2.0f)));
             
 
+
 		    uint32_t Pixel  = (red << 16) | (green << 8) | blue;
 			bitmap[i + TEXTURE_SIZE *j] = Pixel;
         }
@@ -329,40 +342,78 @@ void generate_gradient_texture (uint32_t* bitmap) {
 }
 
 
+
+struct Window_Context {
+    Game_state* state;
+    BITMAPINFO* info;
+    RECT* rect;
+};
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM w, LPARAM l) {
-    if(msg == WM_DESTROY) PostQuitMessage(0);
+
+    Window_Context* ctx = (Window_Context*)GetWindowLongPtrW(hwnd, GWLP_USERDATA);
+
+   switch (msg) {
+     case WM_SIZE: {
+            
+            int width = LOWORD(l);
+            int height = HIWORD(l);
+
+            if (width > 0 && height > 0) {
+                ctx->state->render_w = width;
+                ctx->state->render_h = height;
+
+                if (ctx->state->framebuffer) 
+                    free(ctx->state->framebuffer);
+                
+                ctx->state->framebuffer = (uint32_t*)malloc(4 * width * height);
+
+                ctx->info->bmiHeader.biWidth = width;
+                ctx->info->bmiHeader.biHeight = -height;
+
+
+            }
+        } break;
+        case WM_DESTROY:
+            running = false;
+        PostQuitMessage(0);
+        return 0;
+    }
     return DefWindowProcW(hwnd, msg, w, l);
 }
 
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prev, LPSTR cmd, int show) {
-    WNDCLASSW wc = {0};
+
+    WNDCLASSW wc = { 0 };
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = L"engine";
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+
     RegisterClassW(&wc);
 
-    RECT rect = { 0, 0, RENDER_W , RENDER_H };
-    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
 
-    HWND hwnd = CreateWindowExW(0, L"engine", L"DOD Engine",
-        WS_OVERLAPPEDWINDOW, 100, 100,
-        rect.right - rect.left,   // adjusted width
-        rect.bottom - rect.top,   // adjusted height
-        NULL, NULL, hInstance, NULL);
 
-    ShowWindow(hwnd, show);
+    
+    Game_state state;
+    init_Player(state.player);
+    init_Textures(state.textures);
+    state.framebuffer = (uint32_t*)malloc(4 * state.render_w * state.render_h);
 
-	//populate BITMAPINFO
+    //populate BITMAPINFO
     BITMAPINFO info = { 0 };
     info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    info.bmiHeader.biWidth = RENDER_W;
-    info.bmiHeader.biHeight = -RENDER_H;
+    info.bmiHeader.biWidth = state.render_w;
+    info.bmiHeader.biHeight = -state.render_h;
     info.bmiHeader.biPlanes = 1;
     info.bmiHeader.biBitCount = 32;
     info.bmiHeader.biCompression = BI_RGB;
 
-
-    //populate BITMAPINFO
+    //populate texture BITMAPINFO 
     BITMAPINFO texture_info = { 0 };
     texture_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
     texture_info.bmiHeader.biWidth = TEXTURE_SIZE;
@@ -370,34 +421,72 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prev, LPSTR cmd, int show) {
     texture_info.bmiHeader.biPlanes = 1;
     texture_info.bmiHeader.biBitCount = 32;
     texture_info.bmiHeader.biCompression = BI_RGB;
+
+
+
+    RECT rect = { 0, 0, state.render_w , state.render_h };
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    HWND hwnd = CreateWindowExW(0, L"engine", L"DOD Engine",
+        WS_OVERLAPPEDWINDOW, 100, 100,
+        rect.right - rect.left,   
+        rect.bottom - rect.top,   
+        NULL, NULL, hInstance, NULL);
+
+
+
+    Window_Context ctx;
+    ctx.state = &state;
+    ctx.info = &info;
+    ctx.rect = &rect;
     
+
+    SetWindowLongPtrW(hwnd, GWLP_USERDATA, (LONG_PTR)&ctx);
+
+    ShowWindow(hwnd, show);
+
+	
+
+    
+   
         
-    Game_state state;
 
   
-    init_Player(state.player);
-    init_Textures(state.textures);
-    state.framebuffer=(uint32_t*)malloc(4 * RENDER_W * RENDER_H);
+   
+    
    
 
-    
-    clock_t time_start = clock();
-    clock_t time_end = clock();
+    clock_t game_start = clock();
+    clock_t game_end;
+
+    clock_t frame_start = clock();
+    clock_t frame_end;
+
+    int fps, total_frames=0,avg_fps;
     float delta_time;
 
+
+
+  
     float player_x_change , player_y_change , player_angle_change;
+
     MSG msg = {0};
-    while (1) {
+
+    while (running) {
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT) return 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }
 
-        time_end = clock();
-        delta_time = (float)(time_end - time_start) / CLOCKS_PER_SEC;
-        time_start = time_end;
         
+        frame_end = clock();
+        delta_time = (float)(frame_end - frame_start) / CLOCKS_PER_SEC;
+        frame_start = frame_end;
+        
+        fps = 1.0f / delta_time; 
+        total_frames++;
+
         player_x_change = 0, 
         player_y_change = 0;
         player_angle_change = 0;
@@ -436,7 +525,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prev, LPSTR cmd, int show) {
             state.player.y += player_y_change;
         }
         else {
-            // when 
+            
             player_x_change/=4;
             player_y_change /= 4;
 
@@ -453,22 +542,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prev, LPSTR cmd, int show) {
         
 
        
-
-
         
-
-
-        // player.angle += 1.0f;
         render(state);
 		
 
         HDC dc = GetDC(hwnd);
-          StretchDIBits(dc, 0, 0, RENDER_W, RENDER_H, 0, 0, RENDER_W, RENDER_H, state.framebuffer, &info, DIB_RGB_COLORS, SRCCOPY);
-         //StretchDIBits(dc, 0, 0, RENDER_W, RENDER_H, 0, 0, TEXTURE_SIZE, TEXTURE_SIZE, state.textures.gradient, &texture_info, DIB_RGB_COLORS, SRCCOPY);
+         StretchDIBits(dc, 0, 0, state.render_w, state.render_h, 0, 0, state.render_w, state.render_h, state.framebuffer, &info, DIB_RGB_COLORS, SRCCOPY);
+       //  StretchDIBits(dc, 0, 0, state.render_w, state.render_h, 0, 0, TEXTURE_SIZE, TEXTURE_SIZE, state.textures.gradient, &texture_info, DIB_RGB_COLORS, SRCCOPY);
 
         ReleaseDC(hwnd, dc);
     }
 
+
+    free(state.framebuffer);
+    free(state.textures.gradient);
+    free(state.textures.vertical_lines);
+    free(state.textures.horizontal_lines);
+    free(state.textures.grid_lines);
+
+    game_end = clock();
+    int sesion_time=(float)(game_start - game_end) / CLOCKS_PER_SEC;
+    avg_fps = total_frames / sesion_time;
 
 
 
